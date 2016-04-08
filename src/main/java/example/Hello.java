220b33a3208d1d55934206a8233b29f67378f013
farahcp <chris@mediarails.com>
2016-04-08 12:14:04 -0700

    final working version

--- src/main/java/example/Hello.java
+++ src/main/java/example/Hello.java
@@ -1,8 +1,20 @@
 package example;
 
-/**
- * Created by christopherfarah on 4/8/16.
- */
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.s3.event.S3EventNotification;
+
+import java.util.List;
+
 public class Hello
 {
+    public String myHandler(S3EventNotification request, Context context)
+    {
+        List<S3EventNotification.S3EventNotificationRecord> records = request.getRecords();
+        S3EventNotification.S3EventNotificationRecord record = records.get(0);
+        String bucketName = record.getS3().getBucket().getName();
+        LambdaLogger logger = context.getLogger();
+        logger.log("received bucket : " + bucketName);
+        return bucketName;
+    }
 }
